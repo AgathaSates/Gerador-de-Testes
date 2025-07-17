f5e5b5090b40375182745585a39d050587d15d82
Agatha Sates <satesagatha@gmail.com>
2025-07-17 00:34:44 -0300

    feat: Adicionado base compartilhado do dominio

---
+++ Gerador-de-Testes.Dominio/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,6 @@
+﻿namespace Gerador_de_Testes.Dominio.Compartilhado;
+public abstract class EntidadeBase<T>
+{
+    public Guid Id { get; set; }
+    public abstract void Atualizar(T registroEditado);
+}
---
+++ Gerador-de-Testes.Dominio/Compartilhado/IRepositorio.cs
@@ -0,0 +1,13 @@
+﻿namespace Gerador_de_Testes.Dominio.Compartilhado;
+public interface IRepositorio<T> where T : EntidadeBase<T>
+{
+    public void Cadastrar(T novoRegistro);
+
+    public bool Editar(Guid idRegistro, T registroEditado);
+
+    public bool Excluir(Guid idRegistro);
+
+    public List<T> SelecionarTodos();
+
+    public T? SelecionarPorId(Guid idRegistro);
+}
