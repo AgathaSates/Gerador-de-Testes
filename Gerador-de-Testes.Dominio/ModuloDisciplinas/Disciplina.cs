351c12823698543e2c12055c853d79357e1f1e8b
AlexAraldi <alexanderaraldi@gmail.com>
2025-07-17 17:21:24 -0300

    feat: implementado domínio de Discilplina

---
+++ Gerador-de-Testes.Dominio/ModuloDisciplinas/Disciplina.cs
@@ -0,0 +1,27 @@
+﻿using Gerador_de_Testes.Dominio.Compartilhado;
+
+namespace Gerador_de_Testes.Dominio.Disciplina
+{
+    public class Disciplina : EntidadeBase<Disciplina>
+    {
+        public string Nome { get; set; }
+
+        public  Disciplina() { }
+
+
+        public Disciplina(string nome) : this()
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+        }
+       
+        public override void AtualizarRegistro(Disciplina registroEditado)
+        {
+            Nome = registroEditado.Nome;
+        }
+
+
+
+
+    }
+}
---
+++ Gerador-de-Testes.Dominio/ModuloDisciplinas/IRepositorioDisciplina.cs
@@ -0,0 +1,8 @@
+﻿using Gerador_de_Testes.Dominio.Compartilhado;
+
+namespace Gerador_de_Testes.Dominio.ModuloDisciplinas
+{
+    public interface IRepositorioDisciplina : IRepositorio<Disciplina>
+    {
+    }
+}
