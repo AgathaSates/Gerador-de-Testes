9c7b9ed78955ed5e68ed0ce1557b720f64cde77f
AlexAraldi <alexanderaraldi@gmail.com>
2025-07-17 17:22:03 -0300

    feat: implementado infraestrutura Disciplina

---
+++ Gerador-de-Testes.Infraestrutura.Orm/ModuloDisciplinas/RepositorioDisciplina.cs
@@ -0,0 +1,20 @@
+﻿using Gerador_de_Testes.Dominio.Disciplina;
+using Gerador_de_Testes.Dominio.Disciplinas;
+using Gerador_de_Testes.Infraestrutura.Orm.Compartilhado;
+
+namespace Gerador_de_Testes.Infraestrutura.Orm.ModuloDisciplinas
+{
+    public class RepositorioDisciplina : RepositorioBaseOrm<Disciplina>, IRepositorioDisciplina
+    {
+        public RepositorioDisciplina(ContextoDados contexto) : base(contexto) { }
+        protected override List<Disciplina> ObterRegistros()
+        {
+            return Contexto.Disciplinas;
+        }
+    }
+
+}
+
+
+
+
