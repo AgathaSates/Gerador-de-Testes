7c9d97403c2e7f6daf7e1633867cd83bd6dec3cd
Agatha Sates <satesagatha@gmail.com>
2025-07-17 01:23:53 -0300

    feat: Adicionado "Logs globais" ao projeto

---
+++ Gerador-de-Testes/ActionFilters/LogarAcaoAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Gerador_de_Testes.ActionFilters;
+
+public class LogarAcaoAttribute : ActionFilterAttribute
+{
+    private readonly ILogger<LogarAcaoAttribute> _logger;
+
+    public LogarAcaoAttribute(ILogger<LogarAcaoAttribute> logger)
+    {
+        _logger = logger;
+    }
+
+    public override void OnActionExecuted(ActionExecutedContext context)
+    {
+        var result = context.Result;
+
+        if (result is ViewResult viewResult && viewResult.Model is not null)
+        {
+            _logger.LogInformation(
+             "\n==========================================================\n" +
+             "    !!!!  ACAO DE ENDPOINT EXECUTADA COM SUCESSO  !!!!\n" +
+             "----------------------------------------------------------\n" +
+             ">>> MODELO RETORNADO:\n>>> {@Modelo}\n" +
+             "==========================================================\n", viewResult.Model);
+        }
+
+        base.OnActionExecuted(context);
+    }
+}
