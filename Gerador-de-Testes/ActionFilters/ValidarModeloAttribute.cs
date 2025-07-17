95e5f4b1f81631a3dfc4f86b9c8fc57f72532b4e
Agatha Sates <satesagatha@gmail.com>
2025-07-17 01:25:59 -0300

    feat: Adicionado "Validador de modelo global" ao projeto

---
+++ Gerador-de-Testes/ActionFilters/ValidarModeloAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Gerador_de_Testes.ActionFilters;
+
+public class ValidarModeloAttribute : ActionFilterAttribute
+{   // Lógica ANTES da execução de cada método Action
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var modelState = context.ModelState;
+
+        if (!modelState.IsValid)
+        {
+            var controller = (Controller)context.Controller;
+
+            var viewModel = context.ActionArguments
+                .Values
+                .FirstOrDefault(x => x.GetType().Name.EndsWith("ViewModel"));
+
+            context.Result = controller.View(viewModel);
+        }
+    }
+}
