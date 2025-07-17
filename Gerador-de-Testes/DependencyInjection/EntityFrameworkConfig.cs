58cd6368bd1b43797c45364308f4ce3cd18a51fb
Agatha Sates <satesagatha@gmail.com>
2025-07-17 01:31:46 -0300

    feat: Adicionado configuração de conexão com o banco de dados

---
+++ Gerador-de-Testes/DependencyInjection/EntityFrameworkConfig.cs
@@ -0,0 +1,15 @@
+﻿using Gerador_de_Testes.Infraestrutura.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gerador_de_Testes.DependencyInjection;
+
+public static class EntityFrameworkConfig
+{
+    public static void AddEntityFrameworkConfig(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration["SQL_CONNECTION_STRING"];
+
+        services.AddDbContext<GeradorDeTestesDbContext>(options =>
+            options.UseSqlServer(connectionString));
+    }
+}
