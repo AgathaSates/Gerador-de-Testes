1c4fc2cfd5a5a3597077be82045b2b3a7b833a4b
Agatha Sates <satesagatha@gmail.com>
2025-07-17 02:59:18 -0300

    feat: Adicionado base da página inicial

---
+++ Gerador-de-Testes/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gerador_de_Testes.WebApp.Controllers;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
