b9ac9b62bcb0e66c48391b0fe63e718046be5cd6
Agatha Sates <satesagatha@gmail.com>
2025-07-17 00:59:51 -0300

    feat: Adicionado contexto do bando de dados

---
+++ Gerador-de-Testes.Infraestrutura.Orm/Compartilhado/GeradorDeTestesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gerador_de_Testes.Infraestrutura.Orm.Compartilhado;
+public class GeradorDeTestesDbContext : DbContext
+{
+    // public DbSet<Entidade> Entidades { get; set; } *adicionar DbSet para cada entidade nova
+
+    public GeradorDeTestesDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var assembly = typeof(GeradorDeTestesDbContext).Assembly;
+
+        modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
