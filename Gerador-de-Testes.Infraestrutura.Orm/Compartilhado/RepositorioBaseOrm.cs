c139b5657a2d1b902b7ea53184a5c3580b22dac9
Agatha Sates <satesagatha@gmail.com>
2025-07-17 01:00:30 -0300

    feat: Adicionado repositorio base em Orm

---
+++ Gerador-de-Testes.Infraestrutura.Orm/Compartilhado/RepositorioBaseOrm.cs
@@ -0,0 +1,52 @@
+﻿using Gerador_de_Testes.Dominio.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gerador_de_Testes.Infraestrutura.Orm.Compartilhado;
+public class RepositorioBaseOrm<T> where T: EntidadeBase<T>
+{
+    protected readonly DbSet<T> _registros;
+
+    public RepositorioBaseOrm(GeradorDeTestesDbContext contexto)
+    {
+        _registros = contexto.Set<T>();
+    }
+
+    public void Cadastrar(T novoRegistro)
+    {
+        _registros.Add(novoRegistro);
+    }
+
+    public bool Editar(Guid idRegistro, T registroEditado)
+    {
+        var registroSelecionado = SelecionarPorId(idRegistro);
+
+        if (registroSelecionado is null)
+            return false;
+
+        registroSelecionado.Atualizar(registroEditado);
+
+        return true;
+    }
+
+    public bool Excluir(Guid idRegistro)
+    {
+        var registroSelecionado = SelecionarPorId(idRegistro);
+
+        if (registroSelecionado is null)
+            return false;
+
+        _registros.Remove(registroSelecionado);
+
+        return true;
+    }
+
+    public virtual T? SelecionarPorId(Guid idRegistro)
+    {
+        return _registros.FirstOrDefault(x => x.Id.Equals(idRegistro));
+    }
+
+    public virtual List<T> SelecionarTodos()
+    {
+        return _registros.ToList();
+    }
+}
