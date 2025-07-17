3983deaf73172049a52f258d98dea733a57853e3
Agatha Sates <satesagatha@gmail.com>
2025-07-17 01:53:45 -0300

    feat: Adicionado "Migrador de dados automático"

---
+++ Gerador-de-Testes/Orm/DatabaseOperations.cs
@@ -0,0 +1,14 @@
+﻿using Gerador_de_Testes.Infraestrutura.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gerador_de_Testes.Orm;
+
+public static class DatabaseOperations
+{
+    public static void ApplyMigrations(this IHost app)
+    {
+        var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<GeradorDeTestesDbContext>();
+        dbContext.Database.Migrate();
+    }
+}
