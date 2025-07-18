eac50002452491a251408e875601497c1830857d
AlexAraldi <alexanderaraldi@gmail.com>
2025-07-18 14:07:44 -0300

    feat: implementado Extensions e ViewModel de Disciplina

--- Gerador-de-Testes/Extensions/DisciplinaExtensions.cs
+++ Gerador-de-Testes/Extensions/DisciplinaExtensions.cs
@@ -1,5 +1,18 @@
-﻿namespace Gerador_de_Testes.WebApp.Extensions;
+﻿using Gerador_de_Testes.Dominio.Disciplina;
+using Gerador_de_Testes.WebApp.Models;
 
-public static class DisciplinaExtensions
+namespace Gerador_de_Testes.WebApp.Extensions
 {
+    public static class DisciplinaExtensions
+    {
+        public static Disciplina ParaEntidade(this FormularioDisciplinaViewModel formularioVM)
+        {
+            return new Disciplina(nome: formularioVM.Nome);
+        }
+        public static CadastrarDisciplinaViewModel ParaCadastrarVM(this Disciplina disciplina)
+        {
+            return new CadastrarDisciplinaViewModel(disciplina.Nome);
+        }
+    }
 }
+
--- Gerador-de-Testes/Models/DisciplinaViewModel.cs
+++ Gerador-de-Testes/Models/DisciplinaViewModel.cs
@@ -1,5 +1,75 @@
-﻿namespace Gerador_de_Testes.WebApp.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class DisciplinaViewModel
+namespace Gerador_de_Testes.WebApp.Models;
+
+public class FormularioDisciplinaViewModel 
 {
+    [Required(ErrorMessage = "O campo \"Nome\" é obrigatório.")]
+    [MinLength(3, ErrorMessage = "O campo \"Nome\" precisa conter ao menos 3 caracteres.")]
+    [MaxLength(100, ErrorMessage = "O campo \"Nome\" precisa conter no máximo 100 caracteres.")]
+    public string Nome { get; set; }
 }
+
+public class CadastrarDisciplinaViewModel : FormularioDisciplinaViewModel
+{
+    public CadastrarDisciplinaViewModel() { }
+    public CadastrarDisciplinaViewModel(string nome)
+    {
+        Nome = nome;
+    }
+}
+public class EditarDisciplinaViewModel : FormularioDisciplinaViewModel
+{
+    public int Id { get; set; }
+    public EditarDisciplinaViewModel() { }
+    public EditarDisciplinaViewModel(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+public class ExcluirDisciplinaViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public ExcluirDisciplinaViewModel() { }
+    public ExcluirDisciplinaViewModel(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+public class VisualizarDisciplinaViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public VisualizarDisciplinaViewModel() { }
+    public VisualizarDisciplinaViewModel(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+public class DetalharDisciplinaViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public DetalharDisciplinaViewModel() { }
+    public DetalharDisciplinaViewModel(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+public class SelecionarDisciplinaViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public SelecionarDisciplinaViewModel() { }
+    public SelecionarDisciplinaViewModel(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+
