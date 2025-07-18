1ea819e7e622e6f8b6fb4394694ceb6312712c8f
Agatha Sates <satesagatha@gmail.com>
2025-07-18 18:21:24 -0300

    fix: Correção do contrutor e usings no repositorio *Disciplina*

--- Gerador-de-Testes.Infraestrutura.Orm/ModuloDisciplinas/RepositorioDisciplina.cs
+++ Gerador-de-Testes.Infraestrutura.Orm/ModuloDisciplinas/RepositorioDisciplina.cs
@@ -1,18 +1,12 @@
-﻿using Gerador_de_Testes.Dominio.Disciplina;
-using Gerador_de_Testes.Dominio.Disciplinas;
+﻿using Gerador_de_Testes.Dominio.ModuloDisciplinas;
 using Gerador_de_Testes.Infraestrutura.Orm.Compartilhado;
 
 namespace Gerador_de_Testes.Infraestrutura.Orm.ModuloDisciplinas
 {
     public class RepositorioDisciplina : RepositorioBaseOrm<Disciplina>, IRepositorioDisciplina
     {
-        public RepositorioDisciplina(ContextoDados contexto) : base(contexto) { }
-        protected override List<Disciplina> ObterRegistros()
-        {
-            return Contexto.Disciplinas;
-        }
+        public RepositorioDisciplina(GeradorDeTestesDbContext contexto) : base(contexto) { }
     }
-
 }
 
 
