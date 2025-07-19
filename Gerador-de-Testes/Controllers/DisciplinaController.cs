4b69accc0dba01e77cbe157851b224a2d6dfe44e
Agatha Sates <satesagatha@gmail.com>
2025-07-19 00:39:45 -0300

    fix: Corrigido nome da rota de *Disciplina*

--- Gerador-de-Testes/Controllers/DisciplinaController.cs
+++ Gerador-de-Testes/Controllers/DisciplinaController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Gerador_de_Testes.WebApp.Controllers;
 
-[Route("disciplina")]
+[Route("disciplinas")]
 public class DisciplinaController : Controller
 {
     private readonly GeradorDeTestesDbContext _contexto;
