c1bfd86022ddbb25c52b276f8fb873eae2ba277d
Agatha Sates <satesagatha@gmail.com>
2025-07-20 05:52:42 -0300

    feat: Adicionado repositorio de *Teste*

---
+++ Gerador-de-Testes.Infraestrutura.Orm/ModuloTeste/RepositorioTeste.cs
@@ -0,0 +1,26 @@
+﻿using Gerador_de_Testes.Dominio.ModuloTeste;
+using Gerador_de_Testes.Infraestrutura.Orm.Compartilhado;
+
+namespace Gerador_de_Testes.Infraestrutura.Orm.ModuloTeste;
+public class RepositorioTeste : RepositorioBaseOrm<Teste>, IRepositorioTeste
+{
+    public RepositorioTeste(GeradorDeTestesDbContext contexto) : base(contexto) { }
+
+    public Teste DuplicarTeste(Guid idTeste)
+    {
+        var testeOriginal = SelecionarPorId(idTeste);
+        
+        string novoTitulo = $"{testeOriginal.Titulo} - (Cópia)";
+
+        var testeDuplicado = new Teste(
+            novoTitulo,
+            testeOriginal.Disciplina,
+            testeOriginal.Materias,
+            testeOriginal.QuantidadeQuestoes,
+            testeOriginal.Serie,
+            testeOriginal.ProvaRecuperacao        
+        );
+
+        return testeDuplicado;
+    }
+}
