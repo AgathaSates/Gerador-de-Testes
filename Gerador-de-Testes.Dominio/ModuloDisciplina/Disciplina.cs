93b997d7c32d659d25ad9a6caec95277b7515c01
Agatha Sates <satesagatha@gmail.com>
2025-07-20 05:15:45 -0300

    feat: Adicionado relação entre *Disciplina* e  "Testes e Matérias"

--- Gerador-de-Testes.Dominio/ModuloDisciplina/Disciplina.cs
+++ Gerador-de-Testes.Dominio/ModuloDisciplina/Disciplina.cs
@@ -1,12 +1,20 @@
 ﻿using Gerador_de_Testes.Dominio.Compartilhado;
+using Gerador_de_Testes.Dominio.ModuloMateria;
+using Gerador_de_Testes.Dominio.ModuloTeste;
 
 namespace Gerador_de_Testes.Dominio.ModuloDisciplina;
 
 public class Disciplina : EntidadeBase<Disciplina>
 {
     public string Nome { get; set; }
+    public List<Teste> Testes { get; set; }
+    public List<Materia> Materias { get; set; }
 
-    public Disciplina() { }
+    public Disciplina() 
+    {
+        Testes = new List<Teste>();
+        Materias = new List<Materia>();
+    }
 
     public Disciplina(string nome) : this()
     {
