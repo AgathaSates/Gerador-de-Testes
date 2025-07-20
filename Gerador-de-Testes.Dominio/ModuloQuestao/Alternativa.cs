d73371ce15c11e06d6e87db23801ca50a3704977
Agatha Sates <satesagatha@gmail.com>
2025-07-20 05:19:02 -0300

    feat: Adicionado entidade *Questão* e  *Alternativa*

---
+++ Gerador-de-Testes.Dominio/ModuloQuestao/Alternativa.cs
@@ -0,0 +1,26 @@
+﻿namespace Gerador_de_Testes.Dominio.ModuloQuestao;
+public class Alternativa
+{
+    public Guid Id { get; set; }
+    public string Descricao { get; set; }
+    public bool Correta { get; private set; }
+    public Questao Questao { get; set; }
+
+    public Alternativa() { }
+    public Alternativa(string descricao) : this()
+    {
+        Id = Guid.NewGuid();
+        Descricao = descricao;
+        Correta = false;
+    }
+
+    public void MarcarCorreta() 
+    {
+        Correta = true;
+    }
+
+    public void DesmarcarCorreta() 
+    {
+        Correta = false;
+    }
+}
---
+++ Gerador-de-Testes.Dominio/ModuloQuestao/Questao.cs
@@ -0,0 +1,51 @@
+﻿using Gerador_de_Testes.Dominio.Compartilhado;
+using Gerador_de_Testes.Dominio.ModuloMateria;
+using Gerador_de_Testes.Dominio.ModuloTeste;
+
+namespace Gerador_de_Testes.Dominio.ModuloQuestao;
+public class Questao : EntidadeBase<Questao>
+{   
+    public string Enunciado { get; set; }
+    public Materia Materia { get; set; }
+    public List<Alternativa> Alternativas { get; set; }
+    public List<Teste> Testes { get; set; }
+
+    public Questao()
+    {
+        Testes = new List<Teste>();
+        Alternativas = new List<Alternativa>();
+    }
+
+    public Questao(Materia materia, string enunciado) : this()
+    {
+        Id = Guid.NewGuid();
+        Materia = materia;
+        Enunciado = enunciado;
+    }
+
+    public override void Atualizar(Questao registroEditado)
+    {
+        Materia = registroEditado.Materia;
+        Enunciado = registroEditado.Enunciado;
+        Alternativas = registroEditado.Alternativas;
+    }
+
+    public void AdicionarAlternativa(Alternativa alternativa)
+    {
+        Alternativas.Add(alternativa);
+    }
+
+    public void RemoverAlternativa(Alternativa alternativa)
+    {
+        Alternativas.Remove(alternativa);
+    }
+
+    public void MarcarAlternativaCorreta(Alternativa alternativa)
+    {
+        foreach (var a in Alternativas)
+        {
+            a.DesmarcarCorreta();
+        }
+        alternativa.MarcarCorreta();
+    }
+}
