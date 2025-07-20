89de96cbe2f892ed42f0db1f63343c6540cf1a77
Agatha Sates <satesagatha@gmail.com>
2025-07-20 05:51:27 -0300

    feat: Adicionado interface  *Teste*

---
+++ Gerador-de-Testes.Dominio/ModuloTeste/IRepositorioTeste.cs
@@ -0,0 +1,7 @@
+﻿using Gerador_de_Testes.Dominio.Compartilhado;
+
+namespace Gerador_de_Testes.Dominio.ModuloTeste;
+public interface IRepositorioTeste : IRepositorio<Teste>
+{
+    public Teste DuplicarTeste(Guid idTeste);
+}
