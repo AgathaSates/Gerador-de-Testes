8f01021c8122d3019c8ccb54fe6ab705d3b88704
Agatha Sates <satesagatha@gmail.com>
2025-07-20 05:54:06 -0300

    feat: Adicionado DbSet de Questões, Alternativas e Testes

--- Gerador-de-Testes.Infraestrutura.Orm/Compartilhado/GeradorDeTestesDbContext.cs
+++ Gerador-de-Testes.Infraestrutura.Orm/Compartilhado/GeradorDeTestesDbContext.cs
@@ -1,14 +1,17 @@
 using Gerador_de_Testes.Dominio.ModuloDisciplina;
 using Gerador_de_Testes.Dominio.ModuloMateria;
+using Gerador_de_Testes.Dominio.ModuloQuestao;
+using Gerador_de_Testes.Dominio.ModuloTeste;
 using Microsoft.EntityFrameworkCore;
 
 namespace Gerador_de_Testes.Infraestrutura.Orm.Compartilhado;
 public class GeradorDeTestesDbContext : DbContext
 {
-    // public DbSet<Entidade> Entidades { get; set; } *adicionar DbSet para cada entidade nova
-
     public DbSet<Disciplina> Disciplinas { get; set; }
     public DbSet<Materia> Materias { get; set; }
+    public DbSet<Questao> Questoes { get; set; }
+    public DbSet<Alternativa> Alternativas { get; set; }
+    public DbSet<Teste> Testes { get; set; }
 
     public GeradorDeTestesDbContext(DbContextOptions options) : base(options) { }
 
