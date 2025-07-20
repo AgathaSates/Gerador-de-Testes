0ade79abb6b6d38ba6ccdbd3bef88f02ae2cdc66
Agatha Sates <satesagatha@gmail.com>
2025-07-20 05:33:29 -0300

    feat: Adicionado mapeador de *Questão* e *Alternativa*

---
+++ Gerador-de-Testes.Infraestrutura.Orm/ModuloQuestao/MapeadorAlternativaOrm.cs
@@ -0,0 +1,26 @@
+﻿using Gerador_de_Testes.Dominio.ModuloQuestao;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gerador_de_Testes.Infraestrutura.Orm.ModuloQuestao;
+public class MapeadorAlternativaOrm : IEntityTypeConfiguration<Alternativa>
+{
+    public void Configure(EntityTypeBuilder<Alternativa> builder)
+    {
+        builder.Property(a => a.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(a => a.Descricao)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(a => a.Correta)
+            .IsRequired();
+
+        builder.HasOne(a => a.Questao)
+            .WithMany(q => q.Alternativas)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ Gerador-de-Testes.Infraestrutura.Orm/ModuloQuestao/MapeadorQuestaoOrm.cs
@@ -0,0 +1,30 @@
+﻿using Gerador_de_Testes.Dominio.ModuloQuestao;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gerador_de_Testes.Infraestrutura.Orm.ModuloQuestao;
+public class MapeadorQuestaoOrm : IEntityTypeConfiguration<Questao>
+{
+    public void Configure(EntityTypeBuilder<Questao> builder)
+    {
+        builder.Property(q => q.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(q => q.Enunciado)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.HasOne(q => q.Materia)
+            .WithMany(m => m.Questoes)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(q => q.Alternativas)
+            .WithOne(a => a.Questao)
+            .IsRequired();
+
+        builder.HasMany(q => q.Testes)
+            .WithMany(t => t.Questoes);
+    }
+}
