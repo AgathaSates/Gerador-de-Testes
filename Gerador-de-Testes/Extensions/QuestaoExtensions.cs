62c1682b69bfe87087733577c7e80e9ce45ec8a2
Agatha Sates <satesagatha@gmail.com>
2025-07-22 01:52:17 -0300

    feat: Adicionado Extensions de *Questão*

---
+++ Gerador-de-Testes/Extensions/QuestaoExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Gerador_de_Testes.Dominio.ModuloMateria;
+using Gerador_de_Testes.Dominio.ModuloQuestao;
+using Gerador_de_Testes.WebApp.Models;
+
+namespace Gerador_de_Testes.WebApp.Extensions;
+
+public static class QuestaoExtensions
+{
+    public static Questao ParaEntidade(this FormularioQuestaoViewModel formVM, List<Materia> materias)
+    {
+        Materia? materiaSelecionada = null;
+
+        foreach (var m in materias)
+        {
+            if (m.Id == formVM.MateriaId)
+                materiaSelecionada = m;
+        }
+
+        List<Alternativa> alternativas = new List<Alternativa>();
+
+        foreach (var a in formVM.Alternativas)
+        {
+            alternativas.Add(new Alternativa(a.AlternativaId, a.Descricao, a.Correta));
+        }
+
+        return new Questao(formVM.Enunciado, materiaSelecionada!, alternativas);
+    }
+
+    public static DetalhesQuestaoViewModel DetalhesVM(this Questao questao)
+    {
+        return new DetalhesQuestaoViewModel(questao.Id, questao.Enunciado, questao.Materia.Nome, questao.Alternativas);
+    }
+}
