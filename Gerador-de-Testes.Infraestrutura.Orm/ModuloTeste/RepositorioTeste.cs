558e8af6c92f5a9552355e446a17a9897d1a9fcf
Agatha Sates <satesagatha@gmail.com>
2025-07-23 02:24:16 -0300

    feat: Adicionado includes nos "Seleionar"  de  *Teste*

--- Gerador-de-Testes.Infraestrutura.Orm/ModuloTeste/RepositorioTeste.cs
+++ Gerador-de-Testes.Infraestrutura.Orm/ModuloTeste/RepositorioTeste.cs
@@ -1,10 +1,28 @@
 ﻿using Gerador_de_Testes.Dominio.ModuloTeste;
 using Gerador_de_Testes.Infraestrutura.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gerador_de_Testes.Infraestrutura.Orm.ModuloTeste;
 public class RepositorioTeste : RepositorioBaseOrm<Teste>, IRepositorioTeste
 {
     public RepositorioTeste(GeradorDeTestesDbContext contexto) : base(contexto) { }
+
+    public override Teste? SelecionarPorId(Guid idRegistro)
+    {
+        return _registros.Include(t => t.Disciplina)
+            .Include(t => t.Questoes)
+            .ThenInclude(q => q.Alternativas)
+            .Include(t => t.Materias)
+            .FirstOrDefault(t => t.Id.Equals(idRegistro));
+    }
+
+    public override List<Teste> SelecionarTodos() 
+    {
+        return _registros.Include(t => t.Disciplina)
+            .Include(t => t.Questoes)
+            .ThenInclude(q => q.Alternativas)
+            .Include(t => t.Materias).ToList();
+    }
 
     public Teste DuplicarTeste(Guid idTeste)
     {
@@ -18,7 +36,8 @@ public class RepositorioTeste : RepositorioBaseOrm<Teste>, IRepositorioTeste
             testeOriginal.Materias,
             testeOriginal.QuantidadeQuestoes,
             testeOriginal.Serie,
-            testeOriginal.ProvaRecuperacao        
+            testeOriginal.ProvaRecuperacao,        
+            testeOriginal.Questoes
         );
 
         return testeDuplicado;
