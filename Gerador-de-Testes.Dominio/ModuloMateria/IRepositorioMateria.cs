b134d1bdfe6b4b3502393d2d6b520f8cacea86d5
Agatha Sates <satesagatha@gmail.com>
2025-07-23 02:20:50 -0300

    feat: Adicionado metodo auxiliar em *Materia*

--- Gerador-de-Testes.Dominio/ModuloMateria/IRepositorioMateria.cs
+++ Gerador-de-Testes.Dominio/ModuloMateria/IRepositorioMateria.cs
@@ -3,4 +3,5 @@ using Gerador_de_Testes.Dominio.Compartilhado;
 namespace Gerador_de_Testes.Dominio.ModuloMateria;
 public interface IRepositorioMateria : IRepositorio<Materia>
 {
+    public List<Materia> SelecionarMateriasPorDisciplina(Guid disciplinaId);
 }
